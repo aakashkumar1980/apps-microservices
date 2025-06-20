bc0580a9d57fd944f56ec9da9102b04790df535c
aakash.kumar1980 <aakash,kumar1980@yahoo.com>
2025-06-19 19:56:01 -0700

    fd

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
 plugins {
-    id("java-library")
-    id("maven-publish")
-    id("org.springframework.boot") version "3.5.0"
-    id("io.spring.dependency-management") version "1.1.5"
+  id("java-library")
+  id("maven-publish")
+  id("org.springframework.boot") version "3.5.0"
+  id("io.spring.dependency-management") version "1.1.5"
 }
 
 java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(21))
-    }
+  toolchain {
+    languageVersion.set(JavaLanguageVersion.of(21))
+  }
 }
 
 group = "com.example.tutorial"
@@ -20,29 +20,29 @@ val ioUtilsVersion = "2.19.0"
 val commonsLangVersion = "3.17.0"
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 
-    // Placeholder for company-provided (in-house Artifactory) repository
-    /** maven {
-        url = uri("<company-artifactory-url>")
-        // Authentication should be handled via SSO, do not add username/password here
-    } **/
+  // Placeholder for company-provided (in-house Artifactory) repository
+  /** maven {
+  url = uri("<company-artifactory-url>")
+  // Authentication should be handled via SSO, do not add username/password here
+  } **/
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web:$springBootVersion")
-    implementation("org.apache.commons:commons-lang3:$commonsLangVersion") { exclude(group = "org.slf4j") }
-    implementation("org.apache.commons:commons-io:$ioUtilsVersion")
+  implementation("org.springframework.boot:spring-boot-starter-web:$springBootVersion")
+  implementation("org.apache.commons:commons-lang3:$commonsLangVersion") { exclude(group = "org.slf4j") }
+  implementation("commons-io:commons-io:$ioUtilsVersion")
 }
 
 tasks.named<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
-    archiveFileName.set("${rootProject.name}-${version}.jar")
+  archiveFileName.set("${rootProject.name}-${version}.jar")
 }
 
 tasks.named("build") {
-    dependsOn("bootJar")
+  dependsOn("bootJar")
 }
 
 tasks.test {
-    useJUnitPlatform()
+  useJUnitPlatform()
 }
