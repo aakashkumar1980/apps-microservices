cc7bcd84fc267d5bcf4c0991eee28645a6cca619
aakash.kumar1980 <aakash,kumar1980@yahoo.com>
2025-06-26 00:08:06 -0700

    updated

--- src/main/java/com/example/tutorial/microservices_write/controller/CampaignCommandController.java
+++ src/main/java/com/example/tutorial/microservices_write/controller/CampaignCommandController.java
@@ -1,7 +1,7 @@
 package com.example.tutorial.microservices_write.controller;
 
 import com.example.tutorial.common.dto.campaign.Campaign;
-import com.example.tutorial.microservices_write.controller.service.CampaignCommandService;
+import com.example.tutorial.microservices_write.service.CampaignCommandService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
