ffb10a1b48c4f963cb7c9670aa274e4a0d046f73
aakash.kumar1980 <aakash,kumar1980@yahoo.com>
2025-06-30 16:28:48 -0700

    updated

--- src/main/java/com/example/tutorial/common/configurations/CouchbaseConfiguration.java
+++ src/main/java/com/example/tutorial/common/configurations/CouchbaseConfiguration.java
@@ -1,41 +1,52 @@
 package com.example.tutorial.common.configurations;
 
+import com.couchbase.client.java.env.ClusterEnvironment;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.data.couchbase.config.AbstractCouchbaseConfiguration;
 import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
+@PropertySources(
+    @PropertySource("classpath:application-db.properties")
+)
 public class CouchbaseConfiguration extends AbstractCouchbaseConfiguration {
 
-    @Value("${spring.couchbase.connection-string}")
-    private String connectionString;
+  @Override
+  protected void configureEnvironment(ClusterEnvironment.Builder builder) {
+    builder.securityConfig().enableTls(true);
+  }
 
-    @Value("${spring.couchbase.username}")
-    private String username;
+  @Value("${spring.couchbase.connection-string}")
+  private String connectionString;
 
-    @Value("${spring.couchbase.password}")
-    private String password;
+  @Value("${spring.couchbase.username}")
+  private String username;
 
-    @Value("${spring.couchbase.bucket.name}")
-    private String bucketName;
+  @Value("${spring.couchbase.password}")
+  private String password;
 
-    @Override
-    public String getConnectionString() {
-        return connectionString;
-    }
+  @Value("${spring.couchbase.bucket.name}")
+  private String bucketName;
 
-    @Override
-    public String getUserName() {
-        return username;
-    }
+  @Override
+  public String getConnectionString() {
+    return connectionString;
+  }
 
-    @Override
-    public String getPassword() {
-        return password;
-    }
+  @Override
+  public String getUserName() {
+    return username;
+  }
 
-    @Override
-    public String getBucketName() {
-        return bucketName;
-    }
+  @Override
+  public String getPassword() {
+    return password;
+  }
+
+  @Override
+  public String getBucketName() {
+    return bucketName;
+  }
 }
