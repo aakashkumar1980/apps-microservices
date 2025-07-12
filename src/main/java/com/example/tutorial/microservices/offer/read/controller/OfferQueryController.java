7c7629ec343c92978c7ff3cc43621631a47618ac
aakash.kumar1980 <aakash,kumar1980@yahoo.com>
2025-07-12 14:07:39 -0700

    updated

--- src/main/java/com/example/tutorial/microservices/offer/read/controller/OfferQueryController.java
+++ src/main/java/com/example/tutorial/microservices/offer/read/controller/OfferQueryController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/campaigns")
+@RequestMapping("/api/offers")
 public class OfferQueryController {
 
   private static final Logger log = LoggerFactory.getLogger(OfferQueryController.class);
