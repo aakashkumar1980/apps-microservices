1768c23d941fc8b206c65b53559a4053e0fa68b0
aakash.kumar1980 <aakash,kumar1980@yahoo.com>
2025-07-16 19:24:16 -0700

    adding architecture data 2

--- src/main/java/com/example/tutorial/common/dto/offer/Offer.java
+++ src/main/java/com/example/tutorial/common/dto/offer/Offer.java
@@ -1,81 +1,80 @@
 package com.example.tutorial.common.dto.offer;
 
-import com.example.tutorial.common.dto.reward.RewardType;
-import jakarta.validation.constraints.*;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import org.springframework.data.couchbase.core.mapping.Field;
-
+import jakarta.validation.constraints.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 public class Offer {
 
+  @NotBlank
   @JsonProperty("campaign_id")
-  @Field("campaign_id")
-  @NotBlank(message = "Campaign ID is required")
   private String campaignId;
 
-  @JsonProperty("merchant_id")
-  @Field("merchant_id")
-  @NotBlank(message = "Merchant ID is required")
-  private String merchantId;
+  @NotNull
+  @JsonProperty("type")
+  private OfferType type;
 
-  @JsonProperty("reward_type")
-  @Field("reward_type")
-  @NotNull(message = "Reward type is required")
-  private RewardType rewardType;
+  @NotNull
+  @JsonProperty("status")
+  private OfferStatus status;
 
-  @JsonProperty("reward_value")
-  @Field("reward_value")
-  @DecimalMin(value = "0.01", message = "Reward value must be greater than 0")
-  private BigDecimal rewardValue;
+  @DecimalMin("0.0")
+  @JsonProperty("discount_amount")
+  private BigDecimal discountAmount;
 
-  @JsonProperty("eligibility_criteria")
-  @Field("eligibility_criteria")
-  @NotBlank(message = "Eligibility criteria is required")
-  private String eligibilityCriteria;
+  @NotBlank
+  @Pattern(regexp = "^[A-Z]{3}$", message = "Currency must be ISO 4217 format (e.g., USD, INR)")
+  @JsonProperty("currency")
+  private String currency;
 
-  @JsonProperty("offer_status")
-  @Field("offer_status")
-  @NotNull(message = "Offer status is required")
-  private OfferStatus offerStatus;
-
+  @NotNull
   @JsonProperty("valid_from")
-  @Field("valid_from")
   private LocalDateTime validFrom;
 
+  @NotNull
   @JsonProperty("valid_to")
-  @Field("valid_to")
   private LocalDateTime validTo;
+
+  @Min(0)
+  @JsonProperty("max_redemptions")
+  private Integer maxRedemptions;
 
   public String getCampaignId() { return campaignId; }
   public void setCampaignId(String campaignId) { this.campaignId = campaignId; }
-  public String getMerchantId() { return merchantId; }
-  public void setMerchantId(String merchantId) { this.merchantId = merchantId; }
-  public RewardType getRewardType() { return rewardType; }
-  public void setRewardType(RewardType rewardType) { this.rewardType = rewardType; }
-  public BigDecimal getRewardValue() { return rewardValue; }
-  public void setRewardValue(BigDecimal rewardValue) { this.rewardValue = rewardValue; }
-  public String getEligibilityCriteria() { return eligibilityCriteria; }
-  public void setEligibilityCriteria(String eligibilityCriteria) { this.eligibilityCriteria = eligibilityCriteria; }
-  public OfferStatus getOfferStatus() { return offerStatus; }
-  public void setOfferStatus(OfferStatus offerStatus) { this.offerStatus = offerStatus; }
+
+  public OfferType getType() { return type; }
+  public void setType(OfferType type) { this.type = type; }
+
+  public OfferStatus getStatus() { return status; }
+  public void setStatus(OfferStatus status) { this.status = status; }
+
+  public BigDecimal getDiscountAmount() { return discountAmount; }
+  public void setDiscountAmount(BigDecimal discountAmount) { this.discountAmount = discountAmount; }
+
+  public String getCurrency() { return currency; }
+  public void setCurrency(String currency) { this.currency = currency; }
+
   public LocalDateTime getValidFrom() { return validFrom; }
   public void setValidFrom(LocalDateTime validFrom) { this.validFrom = validFrom; }
+
   public LocalDateTime getValidTo() { return validTo; }
   public void setValidTo(LocalDateTime validTo) { this.validTo = validTo; }
+
+  public Integer getMaxRedemptions() { return maxRedemptions; }
+  public void setMaxRedemptions(Integer maxRedemptions) { this.maxRedemptions = maxRedemptions; }
 
   @Override
   public String toString() {
     return "Offer{" +
-        ", campaignId='" + campaignId + '\'' +
-        ", merchantId='" + merchantId + '\'' +
-        ", rewardType=" + rewardType +
-        ", rewardValue=" + rewardValue +
-        ", eligibilityCriteria='" + eligibilityCriteria + '\'' +
-        ", offerStatus=" + offerStatus +
+        "campaignId='" + campaignId + '\'' +
+        ", type=" + type +
+        ", status=" + status +
+        ", discountAmount=" + discountAmount +
+        ", currency='" + currency + '\'' +
         ", validFrom=" + validFrom +
         ", validTo=" + validTo +
+        ", maxRedemptions=" + maxRedemptions +
         '}';
   }
 }
