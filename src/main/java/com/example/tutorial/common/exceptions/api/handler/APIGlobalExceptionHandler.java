a58007f2493b054a195c3401ee1fe82f2b992a93
aakash.kumar1980 <aakash,kumar1980@yahoo.com>
2025-09-01 20:03:42 -0700

    code refactor

--- src/main/java/com/example/tutorial/common/exceptions/api/handler/APIGlobalExceptionHandler.java
+++ src/main/java/com/example/tutorial/common/exceptions/api/handler/APIGlobalExceptionHandler.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
